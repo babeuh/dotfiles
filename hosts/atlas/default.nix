fd96c038dd9f720fd71b3607e2e619059bcc6317
Babeuh <60193302+babeuh@users.noreply.github.com>
2022-11-06 17:38:25 +0100

    [update]: update flake.lock and linux kernel

--- hosts/atlas/default.nix
+++ hosts/atlas/default.nix
@@ -71,7 +71,6 @@
   # Fixes https://nix-community.github.io/home-manager/index.html#_why_do_i_get_an_error_message_about_literal_ca_desrt_dconf_literal_or_literal_dconf_service_literal
   programs.dconf.enable = true;
 
-  nix.
   # For steam controller etc.
   # hardware.steam-hardware.enable = true;
 
