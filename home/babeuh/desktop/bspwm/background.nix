2cb3ae0b5d77b55d1d3f2a00303935223c7700ee
Babeuh <60193302+babeuh@users.noreply.github.com>
2022-11-12 17:06:20 +0100

    change(background.dir)

--- home/babeuh/desktop/bspwm/background.nix
+++ home/babeuh/desktop/bspwm/background.nix
@@ -2,6 +2,6 @@
   services.random-background = {
     enable = true;
     # TODO: make background change with colorscheme
-    imageDirectory = "/home/babeuh/Pictures/backgrounds";
+    imageDirectory = "%h/Pictures/backgrounds";
   };
 }
