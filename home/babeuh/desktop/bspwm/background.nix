fa40efab1e4c529211df9c2e21c4afff5026ea2c
Babeuh <60193302+babeuh@users.noreply.github.com>
2022-11-12 16:46:42 +0100

    fix(background)

--- home/babeuh/desktop/bspwm/background.nix
+++ home/babeuh/desktop/bspwm/background.nix
@@ -2,6 +2,6 @@
   services.random-background = {
     enable = true;
     # TODO: make background change with colorscheme
-    imageDirectory = "./backgrounds";
+    imageDirectory = "/home/babeuh/Pictures/backgrounds";
   };
 }
--- home/babeuh/desktop/bspwm/default.nix
+++ home/babeuh/desktop/bspwm/default.nix
@@ -3,6 +3,7 @@
     [ ../common ./xidlehook.nix ./betterlockscreen.nix ./background.nix ./rofi.nix ./wm ];
 
   home.packages = with pkgs; [ scrot feh ];
+  home.file."Pictures/backgrounds".source = ./backgrounds;
 
   xsession = {
     enable = true;
