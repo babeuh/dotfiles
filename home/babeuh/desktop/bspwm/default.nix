b5b3af4a22597ff96a4f7e6dfb15784391dd4bd5
Babeuh <60193302+babeuh@users.noreply.github.com>
2022-11-12 16:50:46 +0100

    add(background.credits)

--- home/babeuh/desktop/bspwm/default.nix
+++ home/babeuh/desktop/bspwm/default.nix
@@ -3,6 +3,7 @@
     [ ../common ./xidlehook.nix ./betterlockscreen.nix ./background.nix ./rofi.nix ./wm ];
 
   home.packages = with pkgs; [ scrot feh ];
+  # vettel-ferrari.png  u/Misify  old.reddit.com/r/F1Porn/comments/avswws/
   home.file."Pictures/backgrounds".source = ./backgrounds;
 
   xsession = {
