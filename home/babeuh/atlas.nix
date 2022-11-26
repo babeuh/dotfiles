e85aa38854549dc735e8f17495e8ae7eb14a0d2e
Babeuh <60193302+babeuh@users.noreply.github.com>
2022-11-26 21:07:05 +0100

    change(colorscheme)

--- home/babeuh/atlas.nix
+++ home/babeuh/atlas.nix
@@ -4,6 +4,6 @@
     ./features/games
     ./features/desktop/bspwm
   ];
-  colorscheme = inputs.nix-colors.colorSchemes.gruvbox-dark-hard;
+  colorscheme = inputs.nix-colors.colorSchemes.gruvbox-material-dark-hard;
   wallpaper = ./backgrounds/vettel-years.jpg;
 }
