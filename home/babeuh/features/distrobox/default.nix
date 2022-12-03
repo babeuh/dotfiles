f6efeb0c76759a3a9c4e2f5550f14a085f76dc4a
Babeuh <60193302+babeuh@users.noreply.github.com>
2022-12-03 18:51:52 +0100

    fix(distrobox.xorg)

--- home/babeuh/features/distrobox/default.nix
+++ home/babeuh/features/distrobox/default.nix
@@ -1,3 +1,18 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ distrobox ];
+{ pkgs, config, ... }: {
+  home.packages = with pkgs; [ distrobox xorg.xhost ];
+
+  systemd.user.services.distrobox-xhost = {
+    Unit = {
+      Description = "Configure xhost for distrobox";
+    };
+
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.xorg.xhost}/bin/xhost +si:localuser:${config.home.username}";
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
