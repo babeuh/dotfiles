7ded4f17a429d472989cb9a28bc7df4acb821528
Babeuh <60193302+babeuh@users.noreply.github.com>
2023-03-08 18:44:47 +0100

    modify(babeuh.nvim): plugins/nvim-cmp: change activation

--- home/babeuh/features/cli/vim.nix
+++ home/babeuh/features/cli/vim.nix
@@ -67,16 +67,17 @@
         end,
         '';
         preselect = "None";
-        completion.keyword_length = 1;
-        mapping =  {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        completion.autocomplete = "false"; 
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = false })";
           "<Tab>" = {
             modes = [ "i" "s" ];
             action = ''
               function(fallback)
-                luasnip = require('luasnip')
                 if cmp.visible() then
                   cmp.select_next_item()
+                elseif has_words_before() then
+                  cmp.complete()
                 elseif luasnip.expandable() then
                   luasnip.expand()
                 elseif luasnip.expand_or_jumpable() then
@@ -110,5 +111,11 @@
         ];
       };
     };
+    extraConfigLuaPre = ''
+      local luasnip = require('luasnip')
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end'';
   };
 }
